5e755d6c70e6f17b84fce5103c3d8d92f0195268
Suraaj K S <kssuraaj28@gmail.com>
2024-04-14 11:59:54 -0400

    Rename

--- client.py
+++ client.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-import util
+import skt_tut_util
 import sys
 
-#server_ip = sys.argv[1]
-#server_port = int(sys.argv[2])
+server_ip = sys.argv[1]
+server_port = int(sys.argv[2])
 
-server_ip = "0.0.0.0"
-server_port = 9595
 
-with util.connect(server_ip,server_port) as skt:
+with skt_tut_util.connect(server_ip,server_port) as skt:
     while(True):
         write_data = input()
-        util.send_str(skt,write_data)
-        resp = util.recv_str(skt)
+        skt_tut_util.send_str(skt,write_data)
+        resp = skt_tut_util.recv_str(skt)
         print(resp)
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import threading
-import util
+import skt_tut_util
 
 PORT = 9595
 
@@ -11,14 +11,14 @@ def handle_connection(conn, addr):
     with conn:
         try:
             while True:
-                    recd = util.recv_str(conn)
+                    recd = skt_tut_util.recv_str(conn)
                     print(f"{addr}: {recd}")
-                    util.send_str(conn,f"Thanks for sending {recd}")
-        except util.CommError:
+                    skt_tut_util.send_str(conn,f"Thanks for sending {recd}")
+        except skt_tut_util.CommError:
             print(f"{addr} disconnected")
 
 print(f'Server listening on port {PORT}')
-with util.bind_and_listen(PORT) as s:
+with skt_tut_util.bind_and_listen(PORT) as s:
     while True:
         conn, addr = s.accept()
         t = threading.Thread(target=handle_connection, args=(conn, addr))
